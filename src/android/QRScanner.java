650e3a650cf25544ba7ecaa9e7834fa25f7f03bf
Wiebe Nieuwenhuis <wiebenieuwenhuis@gmail.com>
2017-04-18 21:20:58 +0200

    Android support for Datamatrix & Code128

--- src/android/QRScanner.java
+++ src/android/QRScanner.java
@@ -456,6 +456,8 @@ public class QRScanner extends CordovaPlugin implements BarcodeCallback {
                 //Configure the decoder
                 ArrayList<BarcodeFormat> formatList = new ArrayList<BarcodeFormat>();
                 formatList.add(BarcodeFormat.QR_CODE);
+                formatList.add(BarcodeFormat.CODE_128);
+		        formatList.add(BarcodeFormat.DATA_MATRIX);
                 mBarcodeView.setDecoderFactory(new DefaultDecoderFactory(formatList, null, null));
 
                 //Configure the camera (front/back)
